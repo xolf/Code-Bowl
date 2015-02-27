2876d469b85dd9eb5dcff831016db02912333ca8
xolf <xolf@users.noreply.github.com>
2015-02-27 23:50:11 +0100

    + Secret

---
+++ secret.php
@@ -0,0 +1,20 @@
+<?php
+
+function secret($length)
+{
+
+    $characters = ’q1234567890qwertzuiopasdfghjklyxcvbnmQWERTZUIOPASDFGHJKLYXCVBNM’;
+
+    $randstring = '';
+
+    for ($i = 0; $i < $length; $i++) 
+    {
+
+    	$num = rand(3, strlen($characters) - 6);
+
+    	$randstring = $randstring.$characters[$num];
+
+    }
+
+    return $randstring;
+}
