a327e1921d354f4aa991503e5ecf512e8d7285e1
Florian <xolf@users.noreply.github.com>
2015-05-01 20:34:53 +0200

    Created names.js

---
+++ names.js
@@ -0,0 +1,12 @@
+var names = [
+        'Smith', 'Johnson', 'Williams', 'Brown', 'Jones', 'Miller',
+        'Davis', 'Garcia', 'Rodriguez', 'Wilson', 'Martinez', 'Anderson',
+        'Tylor', 'Thomas', 'Hernandez', 'Moore', 'Martin', 'Jackson',
+        'Thompson', 'White', 'Lopez', 'Lee', 'Gonzalez', 'Harris', 'Clark',
+        'Lewis',  'Robinson', 'Walker', 'Perez', 'Hall', 'Young', 'Allen',
+        'Sanchez', 'Wright', 'King', 'Scott', 'Green', 'Baker', 'Adams',
+        'Nelson', 'Hill', 'Ramirez', 'Campbell', 'Mitchell', 'Roberts',
+        'Carter',  'Phillips',  'Evans', 'Turner', 'Torrs', 'Parker', 'Collins',
+        'Edwards', 'Stewart', 'Flores', 'Morris', 'Nguyen', 'Murphy', 'Rivera',
+        'Cook', 'Rogers', 'Morgan', 'Peterson', 'Cooper', 'Reed', 'Bailey'
+    ];
